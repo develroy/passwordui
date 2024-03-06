c770d4dfe7bd4e4eef0b6458a66fb927f3af5ea4
Elroy <develroy@proton.me>
2024-03-06 17:58:46 -0500

    Create React project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@mui/material";
+const App = () => {
+    return (
+        <div>
+            <Button variant='outlined'>Hello World!</Button>
+        </div>
+    );
+}
+
+export default App;
